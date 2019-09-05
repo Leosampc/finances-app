d6f5e238f02923175629b15ce224cdfc382fe373
Leonardo Cruz <leonardosamp11@gmail.com>
2019-09-05 17:39:51 -0300

    Integrando Redux
    Redux configurado;
    Redux integrado com a Dashboard e comunicando com a API;
    Utilizando métodos applyMiddleware (redux) e promise (redux-promise) para resolver o problema das requisições assíncronas.

--- frontend/src/common/template/footer.jsx
+++ frontend/src/common/template/footer.jsx
@@ -4,7 +4,7 @@ export default props => (
     <footer className='main-footer'>
         <strong>
             Copyright &copy; 2019
-            <a href='https://github.com/leosampc' target='_blank'>Leonardo Cruz</a>.
+            <a href='https://github.com/leosampc' target='_blank'> Leonardo Cruz</a>.
         </strong>
     </footer>
 )
